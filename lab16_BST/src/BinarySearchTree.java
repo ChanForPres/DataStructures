6475644c3178a5786a025a01b318b39f7df8fcae
eunjincho503 <choe@carleton.edu>
2015-11-09 13:37:15 +0900

    added contains method

--- lab16_BST/src/BinarySearchTree.java
+++ lab16_BST/src/BinarySearchTree.java
@@ -1,5 +1,33 @@
 /**
  * Created by EunjinCho on 11/9/15.
  */
+
+
 public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> {
+
+    public boolean contains(T key) {
+        TreeNode t = this.myRoot;
+        return contains(t, key);
+    }
+
+    private boolean contains (TreeNode t, T key) {
+        if (t == null) {
+            return false;
+        } else if (t.myItem == key) {
+            return true;
+        } else {
+            if (key.compareTo(t.myItem) < 0) {
+                contains(t.myLeft, key);
+            } else if (key.compareTo(t.myItem) > 0) {
+                contains(t.myRight, key);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<String> t = new BinarySearchTree<>();
+        t.myRoot = t.new TreeNode("a", t.new TreeNode("b"), t.new TreeNode("c"));
+        t.contains("c");
+    }
 }
