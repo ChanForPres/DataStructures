b005228f3fede08ed3e9a4f968dfbf968be55537
eunjincho503 <choe@carleton.edu>
2016-02-23 16:58:34 +0900

    merge sort

--- lab22_ComparisonSorts/readme.md
+++ lab22_ComparisonSorts/readme.md
@@ -15,6 +15,83 @@ Sorting an array or linked list of ```integers``` or any kind of collection of `
 * Terrible: when the array is reversely sorted
 
 #### ```insert``` method
-Implement an insertion sort algorithm on a doubly linked list instead of an array <br \>
-```soFar``` is the first n-items that are already sorted
+* Implement an insertion sort algorithm on a doubly linked list instead of an array
+* ```current``` is the current pointer in the linked list that would be ready to swap
+* ```soFar``` is the linkedlist pointer that we have traversed so far and is sorted already.
+* In short, for every element *e* in the array, we swap the *e* if it's smaller than the element *p* that is in the already sorted list so far.
+* One important part that derived the answer was that the method ```swap()``` returned the ```ListNode```. So the returned ```ListNode``` contains linked list nodes that contain elements that are swapped.
+
 #### Tree Sort (Insertion sort variation)
+The insertion sort algorithm did its insertion in an array, where elements had to be shifted over to make room for the new elements. A **binary search tree** produces a faster insertion. 
+
+* So alternative to insertion sort is to first build the tree through repeated insertions
+* then at the end traverse it in linear time to produce the sorted sequence
+= *tree sort*
+
+### Selection Sort
+Of ```N``` elements, we do the following kind of loop
+```
+for (N):
+    find and remove the smallest element remaining in the collection
+    and add it to the end of another sorted collection
+```
+
+Q. Like insertion sort, is selection sort faster if the array starts off already sorted? No <br \>
+Q. What's the aymptotic runtime of selection sort? <br \>
+The total number of comparisons is ```(N-1) + (N-2) + ... + 1 = N*(N-1)/2``` no matter what the ordering of elements in the array or linked list. Hence it's O(N^2) algorithm. Unlike **insertion sort** that has a better case (already sorted), **selection sort** *doesn't* have a better case. <br \>
+Q. Why do we use selection sort over insertion sort? <br \>
+As a recap, given a list, **selection sort** takes the current element and exchange it with the smallest element on the **right** hand side of the current element. On the other hand, **Insertion sort**, takes the current element and insert it at the proper position of the list on the **left**, adjusting the list every time you insert. <br \>
+Usually, insertion sort performs less comparisons than selection sort. (For already sorted or almost sorted array, insertion sort performs in O(n) times) However, one advantage of selection sort is that the **number of swaps** in selection sort is **O(n)**, while in insertion sort it is in O(n^2). This is useful when sorting on Flash memory, because swaps reduce the lifespan of Flash memory.
+
+#### Heap sort (Selection sort variation)
+Finding the smallest element in the collection usually takes linear time in an array. To quickly find and remove the smallest element, **min heap** would do since it takes O(logN) time to find and remove. 
+
+1. Put all of the numbers in a max heap ```NlogN``` (heapify makes it faster ```N```)
+2. Repeatedly remove the max element from the heap, and store them in an array in that order ```log N```
+
+So overall, runtime is no worse than ```NlogN```. 
+
+### Divide and Conquer
+Both insertion and selection sorting algorithms, have at their basis the structure of **iterating through each item in the collection one-by-one**. However, there's another approach called **divide-and-conquer**. 
+
+#### Basis
+
+1. Split the elements to be sorted into two collections
+2. Sort each collections recursively
+3. Combine the sorted collections
+
+#### Merge Sort
+Merge sort works as follows:
+
+1. Split the collection to be sorted in half somehow
+2. Sort each half (recursively)
+3. Merge the sorted half-lists
+
+It's fast because **merging two lists** that are already sorted takes linear time proportional to the sum of the lengths of the two lists in the worst case.
+
+Each operations of splitting and merging takes N operations, and the levels are in total of 2logN levels, the total time is ```O(NlogN)```.
+
+#### ```mergeSort()```
+* Splitting method: Have two pointers run at the same time. While one pointer ```a``` is moving one at a time, the other pointer ```b``` is moving twice at a time. Once ```b``` reaches the end, the point where ```a``` remains is the splitting point.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lab22_ComparisonSorts/src/IntList.java
+++ lab22_ComparisonSorts/src/IntList.java
@@ -145,9 +145,6 @@ public class IntList {
 		for (ListNode current = myHead; current != null; current = current.myNext) {
 			soFar = insert(current, soFar);
 		}
-        //System.out.println("soFar1: " + soFar.myItem);
-        //System.out.println("soFar2: " + soFar.myNext.myItem);
-        //System.out.println("soFar3: " + soFar.myNext.myNext.myItem);
 		return new IntList(soFar);
 	}
 
@@ -160,12 +157,10 @@ public class IntList {
         ListNode prev;
         for (prev = current; prev != null && prev.getLinkPrev() != null; prev = prev.getLinkPrev()) {
             if (prev.myItem < prev.getLinkPrev().myItem) {
-                // excluding the current itself
                 insertionPointer = swap(prev, prev.getLinkPrev());
             }
         }
 
-        System.out.println();
         return insertionPointer;
 	}
 
@@ -180,14 +175,13 @@ public class IntList {
 
         // make sure node1 -> node2
         if (node1.getLinkPrev() == node2) {
-            System.out.println("node1.getLinkPrev() == node2");
+            //System.out.println("node1.getLinkPrev() == node2");
             ListNode tmp = node2;
             node2 = node1;
             node1 = tmp;
         }
 
         // node1 -> node2
-        System.out.println("swapping");
         ListNode node1Prev = node1.getLinkPrev();
         ListNode node1Next = node1.getLinkNext();
         ListNode node2Prev = node2.getLinkPrev();
@@ -195,11 +189,11 @@ public class IntList {
 
         // 1) node1 => node2Next
         node1.setLinkNext(node2Next);
-        System.out.println("node1: "+node1.myItem);
-        System.out.println("node2: "+node2.myItem);
+        /*System.out.println("node1: "+node1.myItem);
+        System.out.println("node2: "+node2.myItem);*/
         if(node2Next != null) {
-            System.out.println("node2Next, "+node2Next.myItem+ " is not null");
-            System.out.println("node2Next: " + node2Next.myItem + " is next to node1 " + node1.myItem);
+            /*System.out.println("node2Next, "+node2Next.myItem+ " is not null");
+            System.out.println("node2Next: " + node2Next.myItem + " is next to node1 " + node1.myItem);*/
             // 2) node1 <- node2Next
             node2Next.setLinkPrev(node1);
         }
@@ -208,17 +202,17 @@ public class IntList {
         node2.setLinkPrev(node1Prev);
         if (node1Prev != null) {
             // 4) node1Prev => node2
-            System.out.println("node1Prev, "+node1Prev.myItem+ " is not null");
-            System.out.println("node1Prev: " + node1Prev.myItem + " is prev to node2 " + node2.myItem);
+            /*System.out.println("node1Prev, "+node1Prev.myItem+ " is not null");
+            System.out.println("node1Prev: " + node1Prev.myItem + " is prev to node2 " + node2.myItem);*/
             node1Prev.setLinkNext(node2);
         }
 
         // if node1 and node2 is right next to each other
         if (node1.myItem == node2Prev.myItem) {
-            System.out.println("node1 and node2 are right next to each other");
+            //System.out.println("node1 and node2 are right next to each other");
             // 5) node2 <- node1
             node1.setLinkPrev(node2);
-            System.out.println("node2: " + node2.myItem + " is prev to node1 " + node1.myItem);
+            //System.out.println("node2: " + node2.myItem + " is prev to node1 " + node1.myItem);
             // 6) node2 => node1
             node2.setLinkNext(node1);
         } else {
@@ -226,14 +220,14 @@ public class IntList {
             node1.setLinkPrev(node2Prev);
             if (node2Prev != null) {
                 // 8) node2Prev => node1
-                System.out.println("node2Prev: " + node2Prev.myItem + " is prev to node1 " + node1.myItem);
+                //System.out.println("node2Prev: " + node2Prev.myItem + " is prev to node1 " + node1.myItem);
                 node2Prev.setLinkNext(node1);
             }
             // 9) node2 => node1Next
             node2.setLinkNext(node1Next);
             if (node1Next != null) {
                 // 10) node2 <- node1Next
-                System.out.println("node1Next: " + node1Next.myItem + " is next to node1 " + node2.myItem);
+                //System.out.println("node1Next: " + node1Next.myItem + " is next to node1 " + node2.myItem);
                 node1Next.setLinkPrev(node2);
             }
         }
@@ -250,12 +244,13 @@ public class IntList {
 	 * Sorts this list using the selection sort algorithm.
 	 */
 	public void selectionSort() {
+        // create a new sorted collection
 		IntList sorted = new IntList();
 		while (myHead != null) {
 			int minSoFar = myHead.myItem;
 			ListNode minPtr = myHead;
 			// Find the node in the list pointed to by myHead
-			// whose value is largest.
+			// whose value is smallest.
 			for (ListNode p = myHead; p != null; p = p.myNext) {
 				if (p.myItem < minSoFar) {
 					minSoFar = p.myItem;
@@ -293,13 +288,46 @@ public class IntList {
 		if (mySize <= 1) {
 			return this;
 		}
-		IntList oneHalf = new IntList();
-		IntList otherHalf = new IntList();
-		// TODO your code here!
-		return null;
-	}
+		//IntList oneHalf = new IntList();
+		//IntList otherHalf = new IntList();
 
-	/**
+        ListNode toRtn = mergeSort(myHead);
+        IntList finalRtn = new IntList(toRtn);
+        return finalRtn;
+    }
+
+    private ListNode mergeSort(ListNode head) {
+        // no element or only one element (base case)
+        if (head == null || head.getLinkNext() == null) {
+            return head;
+        }
+        // Divide the linked list into two halves
+        else {
+            ListNode slow = head;
+            ListNode fast = head.getLinkNext();
+            while (fast != null) {
+                fast = fast.getLinkNext();
+                if (fast != null) {
+                    slow = slow.getLinkNext();
+                    fast = fast.getLinkNext();
+                }
+            }
+            ListNode oneHalf = head;
+            ListNode otherHalf = slow.getLinkNext();
+            slow.setLinkNext(null);
+            // having two parts, recursively split them
+            ListNode l1 = mergeSort(oneHalf);
+            ListNode l2 = mergeSort(otherHalf);
+
+            // merge together
+            IntList merged = merge(l1, l2);
+
+            return merged.myHead;
+        }
+    }
+
+
+    /**
 	 * Returns the result of merging the two sorted lists / represented by list1
 	 * and list2.
 	 */
@@ -345,7 +373,7 @@ public class IntList {
 		System.out.print("After selection sort: ");
 		System.out.println(values);*/
 
-		values = new IntList();
+		/*values = new IntList();
 		System.out.print("Before insertion sort: ");
 		for (int k = 0; k < 10; k++) {
 			values.addToFront(randomInt());
@@ -353,21 +381,24 @@ public class IntList {
 		System.out.println(values);
 		sortedValues = values.insertionSort();
 		System.out.print("After insertion sort: ");
-		System.out.println(sortedValues);
-        
+		System.out.println(sortedValues);*/
+
         /*values.addToEnd(5);
         values.addToEnd(4);
         values.addToEnd(3);*/
 
 		/*values = new IntList();
 		System.out.print("Before quicksort: ");
-		for (int k = 0; k < 10; k++) {
-			values.addToFront(randomInt());
-		}
+		//for (int k = 0; k < 10; k++) {
+        //    values.addToFront(randomInt());
+		//}
+        values.addToEnd(5);
+        values.addToEnd(4);
+        values.addToEnd(3);
 		System.out.println(values);
 		sortedValues = values.quicksort();
 		System.out.print("After quicksort: ");
-		System.out.println(sortedValues);
+		System.out.println(sortedValues);*/
 
 		values = new IntList();
 		System.out.print("Before merge sort: ");
@@ -377,6 +408,6 @@ public class IntList {
 		System.out.println(values);
 		sortedValues = values.mergeSort();
 		System.out.print("After merge sort: ");
-		System.out.println(sortedValues);*/
+		System.out.println(sortedValues);
 	}
 }
